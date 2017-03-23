05f9f1308bb5a8f75152d3e93645fa61c5e288d9
Carlos Maniero <cmaniero@thoughtworks.com>
2017-03-23 19:05:04 -0300

    App init

---
+++ app/src/main/java/thoughtworks/com/gmapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package thoughtworks.com.gmapp
+
+import android.app.Activity
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webview = findViewById(R.id.webview) as WebView
+        webview.setWebViewClient(WebViewClient())
+        val websettings = webview.settings
+        websettings.javaScriptEnabled = true
+        webview.loadUrl("file:///android_asset/home.html")
+    }
+}
